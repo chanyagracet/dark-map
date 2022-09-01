dc259f32232a6fbd6fedc3289f019cea0ef97908
CHANYA THANGLERDSUMPAN <chanyat@seas.upenn.edu>
2022-09-01 16:14:27 -0400

    dark-map commit

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './map.css';
+
+const Map = () => {
+  return (
+    <div class="embed-container">
+      <iframe 
+        width="1080" 
+        height="720" 
+        frameborder="0" 
+        scrolling="no" 
+        allowfullscreen 
+        src="https://arcg.is/1KrrqP0">
+      </iframe>
+    </div>
+    
+    
+  )
+}
+
+export default Map
